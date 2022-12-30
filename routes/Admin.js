87a1d4fd14bccba7799c53a60dd773aef320ebfd
Jubail Mallick <98705208+Jubail0@users.noreply.github.com>
2022-12-30 14:42:20 +0530

    Update Admin.js

--- routes/Admin.js
+++ routes/Admin.js
@@ -23,23 +23,23 @@ const {
 
 } = require('../controllers/Admin')
 const { verifyToken, isAdmin } = require("../middleware/authenticate")
-// const { isAdmin, verifyToken }= require('../middleware/authenticate')
+
 
 
 // Dashboard
-router.get('/admin/dasboards',dashboardData)
+router.get('/admin/dasboards',verifyToken,isAdmin,verifydashboardData)
 // Orders
-router.get('/admin/orders',ordersData)
-router.get('/admin/orders/:id',singleOrderData)
+router.get('/admin/orders',verifyToken,isAdmin,ordersData)
+router.get('/admin/orders/:id',verifyToken,isAdmin,singleOrderData)
 router.patch('/admin/orders/:id',verifyToken,isAdmin,updateSingleOrder)
 router.delete('/admin/orders/:id',verifyToken,isAdmin,delete_Single_orderInfo)
 // Products
-router.post('/admin/addProduct',upload.single('productImage'),addproduct)
-router.get('/admin/getSubCategories/:categories',getSubCategories)
-router.get('/admin/products',getAllProducts)
-router.get('/admin/products/:id',getSingleProduct)
-router.patch('/admin/products/:id',upload.single('productImage'),updateSingleProduct)
-router.delete('/admin/products/:id',deleteSingleProduct)
+router.post('/admin/addProduct',verifyToken,isAdmin,upload.single('productImage'),addproduct)
+router.get('/admin/getSubCategories/:categories',verifyToken,isAdmin,getSubCategories)
+router.get('/admin/products',verifyToken,isAdmin,getAllProducts)
+router.get('/admin/products/:id',verifyToken,isAdmin,getSingleProduct)
+router.patch('/admin/products/:id',verifyToken,isAdmin,upload.single('productImage'),updateSingleProduct)
+router.delete('/admin/products/:id',verifyToken,isAdmin,deleteSingleProduct)
 // Category
 router.post('/admin/category/:id',verifyToken,isAdmin,createSubCategory)
 router.delete('/admin/category/:id',verifyToken,isAdmin,deleteSubCategory)
